dd6b4f879a99907b5185752249772f3cad064def
Alif Gibran <alife.gibran@rocketmail.com>
2019-03-06 08:57:22 +0700

    user menu

--- ClientApp/src/app/layout/user/components/user-list/user-list.component.ts
+++ ClientApp/src/app/layout/user/components/user-list/user-list.component.ts
@@ -4,7 +4,7 @@ import { User } from '../../models/user';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
 import { AuthService } from 'app/shared/services/auth.service';
 import { Router } from '@angular/router';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { fuseAnimations } from '@fuse/animations';
 import { UserFormComponent } from '../user-form/user-form.component';
 import { PuiSnackbarService } from 'app/shared/pusintek-ui/components/pui-snackbar/pui-snackbar.service';
@@ -19,14 +19,18 @@ import { UserDetailComponent } from '../user-detail/user-detail.component';
   animations: fuseAnimations,
   encapsulation: ViewEncapsulation.None
 })
-export class UserListComponent extends Pagination implements OnInit {
+export class UserListComponent implements OnInit {
 
   users: User[];
   temp: User[];
   selectedUser: string;
+  displayedColumns: string[] = ['Gravatar', 'Nama', 'Nip', 'UserRoles'];
+  dataSource: any
 
   @ViewChild("gravatar") gravatarTpl: TemplateRef<any>;
-  @ViewChild(DatatableComponent) table: DatatableComponent;
+  //@ViewChild(DatatableComponent) table: DatatableComponent;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private userService: UserService,
@@ -34,49 +38,53 @@ export class UserListComponent extends Pagination implements OnInit {
     private router: Router,
     public dialog: MatDialog,
     private snackbarService: PuiSnackbarService
-  ) {
-    super()
-  }
+  ) {}
 
   /** Called by Angular after user-list component initialized */
   ngOnInit() {
-    this.prepareTableContent()
-
+    this.prepareTableContent();
+    //this.dataSource.sort = this.sort;
     // specify column
-    this.columns = [
-      {
-        cellTemplate: this.gravatarTpl,
-        prop: "Gravatar",
-        width: "40"
-      },
-      { prop: "Nama" },
-      { prop: "Nip", width: "170" },
-      { prop: "UserRoles" }
-    ];
+    //this.columns = [
+    //  {
+    //    cellTemplate: this.gravatarTpl,
+    //    prop: "Gravatar",
+    //    width: "40"
+    //  },
+    //  { prop: "Nama" },
+    //  { prop: "Nip", width: "170" },
+    //  { prop: "UserRoles" }
+    //];
   }
 
   prepareTableContent() {
     this.userService.getUsers().subscribe(result => {
       this.users = result;
-      this.temp = [...result];
+      this.dataSource = new MatTableDataSource(this.users)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 
-  searchUser(event: any) {
-    if(event.type == "click") return true
-    const val = event.target.value.toLowerCase();
-
-    const temp = this.temp.filter(function (d) {
-      return (
-        d.Nama.toLowerCase().indexOf(val) !== -1 ||
-        d.Nip.toLowerCase().indexOf(val) !== -1 ||
-        !val
-      );
-    });
-
-    this.users = temp;
-    this.table.offset = 0;
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  //searchUser(event: any) {
+  //  if(event.type == "click") return true
+  //  const val = event.target.value.toLowerCase();
+
+  //  const temp = this.temp.filter(function (d) {
+  //    return (
+  //      d.Nama.toLowerCase().indexOf(val) !== -1 ||
+  //      d.Nip.toLowerCase().indexOf(val) !== -1 ||
+  //      !val
+  //    );
+  //  });
+
+  //  this.users = temp;
+  //  this.table.offset = 0;
+  //}
 
   onAddNewUserClick() {
     let dialogRef = this.dialog.open(UserFormComponent, {
@@ -88,7 +96,7 @@ export class UserListComponent extends Pagination implements OnInit {
       if (result != null) {
         this.userService.postNewUser(result).subscribe(result => {
           this.userService.getUsers().subscribe(result => {
-            this.prepareTableContent();
+            //this.prepareTableContent();
             this.snackbarService.showSnackBar("success", "Tambah User berhasil!");
           });
         });
@@ -96,29 +104,29 @@ export class UserListComponent extends Pagination implements OnInit {
     });
   }
 
-  onAssignRoleClick() {
-    let dialogRef = this.dialog.open(UserDetailComponent, {
-      data: {
-        user: this.selected[0],
-        roles: this.selected[0].UserRoles
-      }
-    });
+  //onAssignRoleClick() {
+  //  let dialogRef = this.dialog.open(UserDetailComponent, {
+  //    data: {
+  //      user: this.selected[0],
+  //      roles: this.selected[0].UserRoles
+  //    }
+  //  });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result != null) {
-        this.userService.postUserRoles(result).subscribe((response: User) => {
-          // search user
-          this.users = this.users.map(x => {
-            if (x.UserId == response.UserId) {
-              x = Object.assign({}, x, response);
-            }
-            return x;
-          });
-          this.snackbarService.showSnackBar();
-        });
-      }
-      //this.animal = result;
-    });
-  }
+  //  dialogRef.afterClosed().subscribe(result => {
+  //    if (result != null) {
+  //      this.userService.postUserRoles(result).subscribe((response: User) => {
+  //        // search user
+  //        this.users = this.users.map(x => {
+  //          if (x.UserId == response.UserId) {
+  //            x = Object.assign({}, x, response);
+  //          }
+  //          return x;
+  //        });
+  //        this.snackbarService.showSnackBar();
+  //      });
+  //    }
+  //    //this.animal = result;
+  //  });
+  //}
 
 }
