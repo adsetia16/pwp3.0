830ae3d973339883e543ed1780b390d36f82fe7d
Alif Gibran <alife.gibran@rocketmail.com>
2019-03-08 16:32:30 +0700

    All Menu Fixed

--- ClientApp/src/app/main/home/components/home/home.component.ts
+++ ClientApp/src/app/main/home/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'main-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: fuseAnimations,
 })
 export class HomeComponent implements OnInit {
 
--- ClientApp/src/app/main/home/home.module.ts
+++ ClientApp/src/app/main/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { HomeRoutingModule } from './home-routing.module';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
     HomeRoutingModule,
+    MatIconModule
   ],
   providers: [
   ],
